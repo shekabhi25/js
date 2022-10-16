4f63edc2b4011a6297af076d09567cdfe29327c0
sheakabhi25 <ag893573@gmail.com>
2022-10-16 15:16:11 +0530

    v2

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 // document.getElementById("count").innerText=5;
-  // let myage=20;
-  // let humandagratio=7;
+// let myage=20;
+// let humandagratio=7;
 
-  // let mydog=myage*humandagratio;
-  // console.log(mydog);
-  let count=0;
+// let mydog=myage*humandagratio;
+// console.log(mydog);
+let count = 0;
 
-  function increment()
-  {
-    count=count+1;
-    
-  console.log(count);
-  document.getElementById("count").innerText=count;
+function increment() {
+  count = count + 1;
 
-  }
+  // console.log(count);
+  document.getElementById("count").innerText = count;
+
+}
+
+
+
+function saveit() {
+  alert("your increment is save upto " + count);
+
+}
+function store()
+{
+
+  
+  // let store=count;
+  let PE= count+ " --> " ;
+  document.getElementById("entryit").innerText=document.getElementById("entryit").innerText+PE;
+}
 
 //   let count2=0;
 
