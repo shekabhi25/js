4674f212c62e058cb54404371d82a970c18436c3
sheakabhi25 <ag893573@gmail.com>
2022-10-16 16:38:34 +0530

    with decrement btn

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-// document.getElementById("count").innerText=5;
-// let myage=20;
-// let humandagratio=7;
 
-// let mydog=myage*humandagratio;
-// console.log(mydog);
 let count = 0;
-
+let prev=0;
 function increment() {
   count = count + 1;
 
   // console.log(count);
-  document.getElementById("count").innerText = count;
+  document.getElementById("count").textContent = count;
 
 }
 
 
+function decrement()
+{
+  count=count-1;
+  document.getElementById("count").textContent = count;
 
+}
 function saveit() {
   alert("your increment is save upto " + count);
 
-}
-function store()
-{
+}  
 
-  
+function store() {
+
+
   // let store=count;
-  let PE= count+ " --> " ;
-  document.getElementById("entryit").innerText=document.getElementById("entryit").innerText+PE;
+  let countel = document.getElementById("entryit");
+  let totalel=document.getElementById("total");
+  let PE = count + " - ";
+
+  countel.textContent = countel.textContent + PE;   // use TextContentalternative(better) of innerText
+
+   totalel.textContent=prev+count;
+   prev=count+prev;
+
+  count=0;
+  document.getElementById("count").textContent = count;
+
 }
-
-//   let count2=0;
-
-//   function countdown()
-//   {
-//     if(count2<10)
-//     {
-//         count2=count2+1;
-//     }
-//     if(count2==10)
-//     {
-//         //document.write;
-//         alert("NOT GREATER THAN 10");
-
-//     }
-
-// document.getElementById("count").innerText=count2;
-//   }
-
-//   countdown();
